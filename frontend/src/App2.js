2f2250a55efc97713c47ed4354077a6ad91323aa
piterm11 <piteralke@gmail.com>
2021-06-06 15:28:58 +0200

    gifs

--- frontend/src/App2.js
+++ frontend/src/App2.js
@@ -1,24 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
-
-function App() {
+function App2() {
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
-					Edit <code>src/App.js</code> and save to reload. D
+					XDDDDDDDDD
 				</p>
 				<a
 					className="App-link"
-					href="app2.js"
+					href="https://reactjs.org"
+					target="_blank"
 					rel="noopener noreferrer"
 				>
-					START
+					Learn React TTWOJA STARA
 				</a>
 			</header>
 		</div>
 	);
 }
 
-export default App;
+export default App2;
