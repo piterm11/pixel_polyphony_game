a0f42f3bcda44d15c23d7ac85c6801d999440dc0
piterm11 <piteralke@gmail.com>
2021-06-06 20:03:46 +0200

    fonty

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { Route, IndexRoute } from 'react-router';
+import App2 from './App2';
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload. D
-				</p>
-				<a
-					className="App-link"
-					href="app2"
-					rel="noopener noreferrer"
-				>
-					START
-				</a>
-			</header>
-		</div>
-	);
+    return (
+        <div className="App">
+            <header className="App-header">
+				<h1 className = "xd">Pixel Polyphony</h1>
+                <a href="App2" className="App-link">xD</a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/App2.js
+++ frontend/src/App2.js
@@ -10,9 +10,7 @@ function App2() {
 				</p>
 				<a
 					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
+					href="App"
 				>
 					Learn React TTWOJA STARA
 				</a>
