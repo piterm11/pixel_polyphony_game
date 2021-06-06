d7207683a2bb854ea27906b4b93b0e2d25468dfe
piterm11 <piteralke@gmail.com>
2021-06-06 19:15:52 +0200

    route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,11 +7,11 @@ function App() {
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
-					Edit <code>src/App.js</code> and save to reload. 
+					Edit <code>src/App.js</code> and save to reload. D
 				</p>
 				<a
 					className="App-link"
-					href="app2.js"
+					href="app2"
 					rel="noopener noreferrer"
 				>
 					START
